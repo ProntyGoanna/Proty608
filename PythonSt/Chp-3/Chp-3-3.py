b697682d9f47404b640434c622729a247ed71f5c
Jihan Zhu <jihan.zhu@gmail.com>
2018-12-16 21:34:10 +1000

    update with more code

---
+++ PythonSt/Chp-3/Chp-3-3.py
@@ -0,0 +1,41 @@
+'''
+Created on 16 Dec. 2018
+
+@author: Jihan
+'''
+
+if __name__ == '__main__':
+    pass
+
+import turtle
+window = turtle.Screen() # Set up the window and its attributes
+window.bgcolor("lightgreen")
+window.title("Tess & Alex")
+tess = turtle.Turtle() # Create tess and set some attributes
+tess.color("hotpink")
+tess.pensize(5)
+
+alex = turtle.Turtle() # Create alex
+
+tess.forward(80) # Make tess draw equilateral triangle
+tess.left(120)
+tess.forward(80)
+
+
+tess.left(120)
+tess.forward(80)
+tess.left(120) # Complete the triangle
+
+tess.right(180) # Turn tess around
+tess.forward(80) # Move her away from the origin
+
+alex.forward(50) # Make alex draw a square
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+
+window.mainloop()
