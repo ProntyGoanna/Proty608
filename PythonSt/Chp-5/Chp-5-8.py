42948bd18f175277ded5dd14ec86dc4d62290e63
Jihan Zhu <jihan.zhu@gmail.com>
2018-12-22 14:47:00 +1000

    check-in more code chp5.

--- PythonSt/Chp-5/Chp-5-8.py
+++ PythonSt/Chp-5/Chp-5-8.py
@@ -7,3 +7,41 @@ Created on 20 Dec. 2018
 if __name__ == '__main__':
     pass
 
+#===============================================================================
+# Replace old with new
+#===============================================================================
+
+def replace_old_with_new(original, old, new):
+    return original.replace(old, new)
+
+print(replace_old_with_new("I am a lumberjack and I am ok!", "am", "Was"))
+
+#===============================================================================
+# Dictionary
+#===============================================================================
+
+inventory = {"apples": 430, "bananas": 312, "oranges": 525, "pears": 217}
+inventory["apples"]=0
+print(inventory)
+inventory["apples"] += 300
+print(inventory, len(inventory))
+
+del inventory["apples"]
+print(inventory, len(inventory))
+
+for key in inventory.keys() :
+    print("got key ", key, "which maps to values ", inventory[key])
+    
+print(inventory.keys())
+#===============================================================================
+# Dictionary short cuts
+#===============================================================================
+for key in inventory:
+    print("got key ", key, "which maps to values ", inventory[key])
+
+print(list(inventory.values()))
+
+print(list(inventory.items()))
+
+for key, value in inventory.items():
+    print("got key ", key, "which maps to values ", inventory[key])    
